a64bcd03c9273542db4fd7e4971ba170d40e9f25
Moti Regev <mregev@synamedia.com>
2024-08-29 23:54:08 +0300

    Player changes suggestion

--- banner.js
+++ banner.js
@@ -1,27 +1,39 @@
 import { init, uiReady } from "senza-sdk";
 import { videoManager } from "./videoManager.js";
-import shaka from "shaka-player";
 
-const TEST_VIDEO = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+const TEST_VIDEO = "https://d1m2jsqth0tf37.cloudfront.net/clear/linear/Nature-Insects_HD/default.mpd";
+
+function getState() {
+	return window.sessionStorage.getItem("state");
+}
+
+function setState(state) {
+	window.sessionStorage.setItem("state", state);
+}
 
 window.addEventListener("load", async () => {
-  try {
-    await init();
-    videoManager.init(new shaka.Player(video));
-    await videoManager.load(TEST_VIDEO);
-    videoManager.play();
-    uiReady();
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		await init();
+		videoManager.init(video);
+		if (!getState()) { // First load
+			await videoManager.load(TEST_VIDEO);
+			videoManager.play();
+			setState("{}");
+		} else {
+			await videoManager.localPlayerLoad(TEST_VIDEO);
+		}
+		uiReady();
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 document.addEventListener("keydown", async function(event) {
 	switch (event.key) {
-    case "Enter": await videoManager.toggleBackground(); break;
-    case "Escape": videoManager.playPause(); break;
-    case "ArrowLeft": videoManager.skip(-30); break;
-    case "ArrowRight": videoManager.skip(30); break;      
+		case "Enter": await videoManager.togglePlayers(); break;
+		case "Escape": videoManager.togglePlayPause(); break;
+		case "ArrowLeft": videoManager.seek(-30); break;
+		case "ArrowRight": videoManager.seek(30); break;
 		default: return;
 	}
 	event.preventDefault();
