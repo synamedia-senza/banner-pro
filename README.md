9174f4f252e2d20242e3ea19c65ce37b788f4e2c
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2025-01-03 12:38:04 +0100

    Using Senza Shaka player.

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Banner
 
-Play video with an advertising banner, and switch between the local and remote players.
+This app demonstrates how to use the Senza version of the Shaka player to play video. When switching between foreground and background mode, the video timecode is automatically synchronized between the local player and the remote player.
 
 See the [Playing Video](https://developer.synamedia.com/senza/docs/playing-video) tutorial in the Senza developer documentation.
+
+## Interface
+
+* OK - toggle between foreground and background mode
+* Back - pause and play the video
+* Left/Right - skip backwards and forwards by 30 seconds
 
 ## Build
 
 ```bash
+npm install --save-dev webpack
 npm ci
 npx webpack -w --config webpack.config.js
 open index.html
--- banner.js
+++ banner.js
@@ -1,28 +1,51 @@
-import { init, uiReady } from "senza-sdk";
-import { videoManager } from "./videoManager.js";
-import shaka from "shaka-player";
+import { init, uiReady, ShakaPlayer, lifecycle } from "senza-sdk";
 
 const TEST_VIDEO = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+
+let player;
 
 window.addEventListener("load", async () => {
   try {
     await init();
-    videoManager.init(new shaka.Player(video));
-    await videoManager.load(TEST_VIDEO);
-    videoManager.play();
+    player = new ShakaPlayer(video);
+    await player.load(TEST_VIDEO);
+    await video.play();
     uiReady();
   } catch (error) {
     console.error(error);
   }
 });
 
-document.addEventListener("keydown", async function(event) {
-	switch (event.key) {
-    case "Enter": await videoManager.toggleBackground(); break;
-    case "Escape": videoManager.playPause(); break;
-    case "ArrowLeft": videoManager.skip(-30); break;
-    case "ArrowRight": videoManager.skip(30); break;      
-		default: return;
-	}
-	event.preventDefault();
+document.addEventListener("keydown", async function (event) {
+  switch (event.key) {
+    case "Enter": await toggleBackground(); break;
+    case "Escape": await playPause(); break;
+    case "ArrowLeft": skip(-30); break;
+    case "ArrowRight": skip(30); break;
+    default: return;
+  }
+  event.preventDefault();
 });
+
+async function toggleBackground() {
+  if (lifecycle.state == lifecycle.UiState.BACKGROUND) {
+    await lifecycle.moveToForeground();
+  } else {
+    // remove this line once the remotePlayer has been updated to sync 
+    // automatically regardless of whether seamless switch is enabled
+    player.remotePlayer.currentTime = video.currentTime;
+    await lifecycle.moveToBackground();
+  }
+}
+
+async function playPause() {
+  if (video.paused) {
+    await video.play();
+  } else {
+    await video.pause();
+  }
+}
+
+function skip(seconds) {
+  video.currentTime = video.currentTime + seconds;
+}
