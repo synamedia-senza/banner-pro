9f11c6fd175e20ae88e29e799453d3f53ebd18f0
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2025-03-10 19:15:52 -0400

    License demo
    Implements four forms of license server authentication, based on examples from https://shaka-player-demo.appspot.com/docs/api/tutorial-license-server-auth.html

--- banner.js
+++ banner.js
@@ -1,8 +1,13 @@
 import * as senza from "senza-sdk";
 
+// Implements four forms of license server authentication, based on examples from
+// https://shaka-player-demo.appspot.com/docs/api/tutorial-license-server-auth.html 
+// Set the auth parameter to no_auth, header_auth, param_auth or cookie_auth
+
 let options = {
-  "url": getParam("url", "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"),
-  "licenseServer": getParam("licenseServer", null),
+  "auth": getParam("auth", "no_auth"),
+  "url": "https://d75lt43xddusd.cloudfront.net/shaka-demo-assets/sintel-widevine/dash.mpd",
+  "licenseServer": "https://cwip-shaka-proxy.appspot.com/no_auth",
   "autoBackground": getParam("autoBackground", "true") == "true",
   "delay": Number(getParam("delay", 15)),
   "maxHeight": Number(getParam("maxHeight", 1080))
@@ -16,6 +21,23 @@ window.addEventListener("load", async () => {
     player = new senza.ShakaPlayer();
     player.configure(playerConfig());
     await player.attach(video);
+    player.getNetworkingEngine().registerRequestFilter((type, request, context) => {
+      if (type == senza.shaka.net.NetworkingEngine.RequestType.LICENSE) {
+        if (options.auth == "header_auth") {
+          banner.innerHTML = "Header Authentication";
+          request.headers['CWIP-Auth-Header'] = 'VGhpc0lzQVRlc3QK';
+        } else if (options.auth == "param_auth") {
+          banner.innerHTML = "Parameter Authentication";
+          request.uris[0] += '?CWIP-Auth-Param=VGhpc0lzQVRlc3QK';
+        } else if (options.auth == "cookie_auth") {
+          banner.innerHTML = "Cookie Authentication";
+          // go to https://cwip-shaka-proxy.appspot.com/set_cookie in the debugger to set the cookie
+          request.allowCrossSiteCredentials = true;
+        } else {
+          banner.innerHTML = "No Authentication";
+        }
+      }
+    });
     await player.load(options.url);
     await video.play();
 
@@ -70,9 +92,12 @@ function getParam(name, defaultValue = null) {
 }
 
 function playerConfig() {
-  let config = {abr: {restrictions: {maxHeight: options.maxHeight}}};
-  if (options.licenseServer) {
-    config.drm = {servers: {"com.widevine.alpha": options.licenseServer}};
-  }
+  let config = {
+    abr: {restrictions: {maxHeight: options.maxHeight}},
+    drm: {
+      servers: {"com.widevine.alpha": options.licenseServer.replace("no_auth", options.auth)},
+      advanced: {'com.widevine.alpha': {'videoRobustness': 'SW_SECURE_CRYPTO', 'audioRobustness': 'SW_SECURE_CRYPTO'}}
+    }
+  };
   return config;
 }
